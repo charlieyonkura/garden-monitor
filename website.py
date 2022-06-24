4080c04c0e78b6e27ffd32e08eb5d095abfa16bb
LinuxLover3000 <charlieyonkura@gmail.com>
2022-06-24 15:07:24 -0400

    Image class

--- website.py
+++ website.py
@@ -5,13 +5,39 @@ from datetime import datetime
 import os
 import exifread
 
-webcamPath = os.path.relpath("static\images")
+imagesPath = os.path.relpath("static\images")
 imagePrefix = "img"
-numberLength = 3 #abcdxxx.jpg -> numberLength = 3
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///test.db"
 db = SQLAlchemy(app)
+
+class Image():
+    images = []
+    def __init__(self, path):
+        self.path = path
+        self.parsePath()
+        self.getDateTime()
+    def parsePath(self):
+        split = os.path.splitext(os.path.split(self.path)[1])
+        name = split[0]
+        self.ext = split[1]
+        i = 0
+        while(not name[i:].isdigit()):
+            i += 1
+        self.number = name[i:]
+        self.prefix = name[:i]
+    def getDateTime(self):
+        tags = exifread.process_file(open(self.path, "rb"))
+        self.datetime = datetime.strptime(tags["Image DateTime"].printable, "%Y:%m:%d %H:%M:%S")
+    def getImages(path, prefix): #returns array of files if prefix matches
+        contents = os.listdir(path)
+        Image.images = [Image(os.path.join(path, image)) for image in contents if image[:len(prefix)] == prefix]
+    def getMostRecent():
+        dates = [image.datetime for image in Image.images]
+        mostRecentDate = max(dates)
+        mostRecentImage = Image.images[dates.index(mostRecentDate)]
+        return mostRecentImage
 
 class Todo(db.Model): #setting columns for database
     id = db.Column(db.Integer, primary_key = True)
@@ -62,15 +88,9 @@ def update(id):
 
 @app.route("/webcam/")
 def webcam():
-    contents = os.listdir(webcamPath)
-    files = [image for image in contents if image[:len(imagePrefix)] == imagePrefix] #if it contains the prefix, allow through filter
-    image = str(max([int(n[len(imagePrefix):-4]) for n in files])) #removes prefix & file extension, returns max image number
-    
-    path = os.path.join(webcamPath, imagePrefix + ("0" * (numberLength - len(image))) + image + files[0][-4:]) #frankenstein together the path
-    tags = exifread.process_file(open(path, "rb"))
-    datetime_obj = datetime.strptime(tags["Image DateTime"].printable, "%Y:%m:%d %H:%M:%S")
-    
-    return render_template("webcam.html", img = path.replace("\\", "/"), date = datetime_obj)
+    Image.getImages(imagesPath, imagePrefix)
+    img = Image.getMostRecent()
+    return render_template("webcam.html", imgPath = img.path.replace("\\", "/"), imgDateTime = img.datetime)
 
 if __name__ == "__main__":
     app.run(debug = True)
