49e3c58c0c7949da7cc9bd616e1547c3d3f1d297
LinuxLover3000 <charlieyonkura@gmail.com>
2022-06-24 23:17:12 -0400

    pass Image objects to html templates

--- website.py
+++ website.py
@@ -33,64 +33,23 @@ class Image():
     def getImages(path, prefix): #returns array of files if prefix matches
         contents = os.listdir(path)
         Image.images = [Image(os.path.join(path, image)) for image in contents if image[:len(prefix)] == prefix]
-    def getMostRecent():
+    def getMostRecent(): #sort list by datetime
         dates = [image.datetime for image in Image.images]
         mostRecentDate = max(dates)
         mostRecentImage = Image.images[dates.index(mostRecentDate)]
         return mostRecentImage
+        #return Image.images[len(Image.images)]
 
-class Todo(db.Model): #setting columns for database
-    id = db.Column(db.Integer, primary_key = True)
-    content = db.Column(db.String(200), nullable = False)
-    date_created = db.Column(db.DateTime, default = datetime.utcnow)
-
-    def __repr__(self): #function to return string
-        return "<Task %r>" % self.id
-
-
-@app.route("/", methods = ["POST", "GET"])
+@app.route("/")
 def index():
-    if request.method == "POST":
-        task_content = request.form["content"] #id from form
-        new_task = Todo(content=task_content)
-        try:
-            db.session.add(new_task) #add to database
-            db.session.commit()
-            return redirect("/")
-        except:
-            return "Error adding task"
-    else:
-        tasks = Todo.query.order_by(Todo.date_created).all() #returns all task objects
-        return render_template("index.html", tasks=tasks) #checks in \templates\
-
-@app.route("/delete/<int:id>")
-def delete(id):
-    task_to_delete = Todo.query.get_or_404(id)
-    try:
-        db.session.delete(task_to_delete)
-        db.session.commit()
-        return redirect("/")
-    except:
-        return "Error deleting task"
-
-@app.route("/update/<int:id>", methods=["POST", "GET"])
-def update(id):
-    task = Todo.query.get_or_404(id)
-    if request.method == "POST":
-        task.content = request.form["content"] #set task's content to input from form
-        try:
-            db.session.commit() #no need to add new entry, only commit changes
-            return redirect("/")
-        except:
-            return "Error updating task"
-    else:
-        return render_template("update.html", task=task)
+    Image.getImages(imagesPath, imagePrefix)
+    image = Image.getMostRecent()
+    return render_template("index.html", img = image)
 
 @app.route("/webcam/")
 def webcam():
     Image.getImages(imagesPath, imagePrefix)
-    img = Image.getMostRecent()
-    return render_template("webcam.html", imgPath = img.path.replace("\\", "/"), imgDateTime = img.datetime)
+    return render_template("webcam.html", imgs = Image.images)
 
 if __name__ == "__main__":
     app.run(debug = True)
