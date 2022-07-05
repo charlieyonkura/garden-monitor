602848df0872613f2f5925773e2fbd696bd98ffa
pagabel48 <pagabel48@gmail.com>
2022-07-05 12:56:00 -0400

    al;sdfjlasdf

--- data.py
+++ data.py
@@ -1,17 +1,31 @@
 import time
 import os
+import threading
+import board
+import adafruit_dht
+import sqlite3
 
-time_delay = 10 # in minutes
-days = 365
 counter = 0
-total_img_count = days * 1440 / time_delay
+sensor = adafruit_dht.DHT11(board.D4)
 
-def take_picture():
-	path = "/home/pi/Desktop/garden-monitor-new/static/images"
-	image_name = 'image'
-	
-	global counter
-	counter +=1
+picture_path = "/home/pi/Desktop/garden-monitor-new/static/images"
+database_path = "/home/pi/Desktop/garden-monitor-new/test.db"
+
+running = True
+take_measurement = True
+take_picture = True
+
+#connect to database, return connection
+def initialize_db():
+	try:
+		database = sqlite3.connect(database_path)
+	except sqlite3.Error as error:
+		print(error.args[0])
+	return database
+
+#take and store image via command line
+def store_picture(path):
+	image_name = "image"
 	
 	if os.path.exists(path) == False:
 		os.mkdir(path)
@@ -19,9 +33,77 @@ def take_picture():
 	data = "fswebcam " + path + "/" + image_name + (str)(counter) + ".jpg"
 	os.system(data)
 	
+# use DHT11
+def find_temp():
+	working = False
+	while not working:
+		try:
+			temperature = sensor.temperature
+			if temperature != None:
+				working = True
+		except RuntimeError as error:
+			print("Error " + error.args[0])
+	return (temperature * 9 / 5) + 32
 	
-while counter < 5:
+def find_humidity():
+	working = False
+	while not working:
+		try:
+			humidity = sensor.humidity
+			if humidity != None:
+				working = True
+		except RuntimeError as error:
+			print("Error " + error.args[0])
+	return humidity
 	
-	take_picture()
+#read humidity and tempurature and store in the database
+def store_data(database):
+	temperature = find_temp()
+	humidity = find_humidity()
 	
-	time.sleep(3) # in seconds
+	sql_command = "INSERT INTO data (humidity, temperature, time) VALUES (%s, %s, datetime('now'));" % (temperature, humidity)
+	
+	try:
+		cursor = database.cursor()
+		cursor.execute(sql_command)
+		database.commit()
+	except sqlite3.Error as error:
+		print(error.args[0])
+		
+#handle timing
+def control_loop():
+	global take_measurement
+	global take_picture
+	while running:
+		time.sleep(300)
+		take_measurement = True
+		time.sleep(300)
+		take_measurement = True
+		take_picture = True
+		
+#responsible for calling functions, does the heavylifting
+def function_loop(database):
+	global take_measurement
+	global take_picture
+	while running:
+		if take_measurement == True:
+			store_data(database)
+			take_measurement = False
+		if take_picture == True:
+			store_picture(picture_path)
+			take_picture = False
+		time.sleep(1)
+			
+#start the program
+def initialize_all():
+	database = initialize_db()
+	
+	YourMomThread = threading.Thread(target=control_loop)
+	
+	YourMomThread.start()
+	function_loop(database)
+	
+	print('::Closing the Datbase')
+	database.close()
+	
+initialize_all()
